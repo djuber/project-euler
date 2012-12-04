664ea4413afb607f69629f1461bea3114aadab2e
Daniel Uber <djuber@gmail.com>
2012-12-04 10:45:53 -0600

    solved problem 47

--- p47.lisp
+++ p47.lisp
@@ -14,3 +14,27 @@
 
 ;; Find the first four consecutive integers to have four distinct primes factors. What is the first of these numbers?
 
+
+(defun number-of-prime-factors (n)
+  (/ (length (prime-powers n)) 2))
+
+(defun numbers-with-k-distinct-prime-factors (k limit)
+  (loop for i from 2 to limit
+       when (= k (number-of-prime-factors i))
+       collect i))
+
+(defun smallest-consecutive-members (n list)
+  (loop for i in list
+       when (every (lambda (e) (member-sorted e list)) (loop for e from i to (1- (+ i n)) collect e))
+collect i))
+
+(defun p47-test-samples ()
+  (values
+   (first (smallest-consecutive-members 2
+					(numbers-with-k-distinct-prime-factors 2 100)))
+   (first (smallest-consecutive-members 3 
+					(numbers-with-k-distinct-prime-factors 3 1000)))))
+
+(defun p47 ()
+  (first (smallest-consecutive-members 4 
+				       (numbers-with-k-distinct-prime-factors 4 200000))))
